1cda0f1bf0d27fdf9652a07a34ed21839ca2e63c
jingws <632397482@qq.com>
2019-07-05 14:54:53 +0800

    歌词样式

--- src/components/player/components/lyric.vue
+++ src/components/player/components/lyric.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lyric-record" :class="{'show' : show}">
-    <div class="lyric">
+    <div ref="lyricWrap" class="lyric">
       <p v-for="(item, index) of lyric" :key="index" :class='{"active" : scrollCtrl(item, index)}'>
         <!-- <span>{{item | splitTime}}</span> -->
         <span>{{item | splitText}}</span>
@@ -28,6 +28,11 @@ export default {
     }
   },
 
+  mounted() {
+    // this.$refs.lyricWrap.scrollTo(0, 0)
+    // console.log(123)
+  },
+
   methods: {
     scrollCtrl(item, index) {
       return this.currentTime >= splitTime(item)
@@ -44,8 +49,8 @@ export default {
 <style lang="scss">
   .lyric-record { width: 100%; height: 128.5vw; color: $color-normal; position: absolute; transition: opacity 300ms linear; opacity: 0; overflow-y: auto;
     &.show { opacity: 1; }
-    >.lyric { width: 100%; padding: 0 6%; box-sizing: border-box; padding-top: calc(50% + 50px);
-      >p { width: 100%; text-align: center; padding: 4% 0; color: rgba(255, 255, 255, .4);
+    >.lyric { width: 100%; padding: 0 6%; box-sizing: border-box; padding: calc(50% + 50px) 0 calc(50% + 50px) 0;
+      >p { width: 100%; text-align: center; padding: 4% 0; color: rgba(255, 255, 255, .4); transition: color 300ms linear;
         &.active { color: $color-normal }
       }
     }
